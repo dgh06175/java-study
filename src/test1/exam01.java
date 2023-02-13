83f9df5215a98dd08fbc9f5b2fc86e78b817fb78
dgh06175 <dgh06175@gmail.com>
2023-02-13 16:15:12 +0900

    first day

---
+++ src/test1/exam01.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class exam01 {
+
+	public static void main(String[] args) {
+		int value = 20;
+		int result = value + 10;
+		System.out.println(result + "입니다.");
+
+	}
+
+}
