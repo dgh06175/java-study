50222055bd1d24d6124dde42f9fd0a7bfec7a0cf
dgh06175 <dgh06175@gmail.com>
2023-09-11 13:20:24 +0900

    문제 4 테스트 통과

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        int len = alpha.length() - 1;
+        System.out.println(len);
+        for (int i = 0; i < word.length(); i++) {
+            boolean isUpper = Character.isUpperCase(word.charAt(i));
+            int index = alpha.indexOf(Character.toLowerCase(word.charAt(i)));
+            if (index == -1) {
+                answer.append(" ");
+                continue;
+            }
+            if (isUpper) answer.append(Character.toUpperCase(alpha.charAt(len - index)));
+            else answer.append(alpha.charAt(len - index));
+        }
+
+        return answer.toString();
     }
 }
