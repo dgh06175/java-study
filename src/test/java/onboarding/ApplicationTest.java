7ccb421a4470e32ec960de2cff59a9ada289627e
dgh06175 <dgh06175@gmail.com>
2023-09-12 18:12:50 +0900

    문제 6 테스트 코드 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -130,6 +130,30 @@ class ApplicationTest {
             List<String> result = List.of("jason@email.com", "jm@email.com", "jpark@email.com", "mj@email.com");
             assertThat(Problem6.solution(forms)).isEqualTo(result);
         }
+
+        @Test
+        void case3() {
+            List<List<String>> forms = List.of(
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@gmail.com", "제이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠")
+            );
+            assertThat(Problem6.solution(forms)).isEqualTo(null);
+        }
+
+        @Test
+        void case4() {
+            List<List<String>> forms = List.of(
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "Jason"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠")
+            );
+            assertThat(Problem6.solution(forms)).isEqualTo(null);
+        }
     }
 
     @Nested
