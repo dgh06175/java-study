3f73ae2d287cdf7a72174342e9197702b840cca8
dgh06175 <dgh06175@gmail.com>
2023-10-10 21:29:33 +0900

    test: 게임 재시작 함수 테스트 코드 추가
    1 입력시 true, 다른 정수 입력시 false, 문자열 입력 시 예외발생

--- src/test/java/baseball/BaseballGameTests.java
+++ src/test/java/baseball/BaseballGameTests.java
@@ -1,21 +1,41 @@
 package baseball;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class BaseballGameTests {
+    BaseballGame b = new BaseballGame();
     @Test
-    @DisplayName("컴퓨터가 랜덤으로 생성하는 숫자가 옳은지 검사하는 함수")
+    @DisplayName("컴퓨터 생성 수 유효성 검사")
     void checkComputerNumber() {
-        BaseballGame b = new BaseballGame();
         List<Integer> result = b.initComputerNumber();
         List<Integer> correctNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
         assertThat(correctNumbers).containsAll(result);
         assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    void inputGameContinue_재시작_검사() {
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
+        assertThat(b.inputGameContinue()).isEqualTo(true);
+
+        System.setIn(new ByteArrayInputStream("2".getBytes()));
+        assertThat(b.inputGameContinue()).isEqualTo(false);
+
+        System.setIn(new ByteArrayInputStream("-421".getBytes()));
+        assertThat(b.inputGameContinue()).isEqualTo(false);
+
+        System.setIn(new ByteArrayInputStream("23".getBytes()));
+        assertThat(b.inputGameContinue()).isEqualTo(false);
+
+        System.setIn(new ByteArrayInputStream("asd".getBytes()));
+        assertThatThrownBy(() -> b.inputGameContinue())
+                .isInstanceOf(InputMismatchException.class);
     }
 }
