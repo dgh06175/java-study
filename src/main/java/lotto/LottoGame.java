4e77802122333df15f22c8039b630c663f7cbaae
dgh06175 <dgh06175@gmail.com>
2023-10-15 23:50:16 +0900

    feat: 구입 금액 입력받고 틀릴시 예외처리 하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.*;
 
 public class LottoGame {
     private Lotto winningLotto;
@@ -11,10 +12,28 @@ public class LottoGame {
 
     public void play() {
         inputMoney();
-        lottoMachine = new LottoMachine(money);
-        lottoArray = lottoMachine.getLottoArray();
-        inputWinningLotto();
-        lottoAnalyzer = new LottoAnalyzer(lottoArray, winningLotto);
-        printResult(lottoAnalyzer.getResult());
+        //lottoMachine = new LottoMachine(money);
+        //lottoArray = lottoMachine.getLottoArray();
+        //inputWinningLotto();
+        //lottoAnalyzer = new LottoAnalyzer(lottoArray, winningLotto);
+        //printResult(lottoAnalyzer.getResult());
+    }
+
+    private void inputMoney() {
+        String inputMoney = Console.readLine();
+        try {
+            validateInputMoney(inputMoney);
+        } catch (IllegalArgumentException e) {
+            // e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void validateInputMoney(String inputMoney) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(inputMoney);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 돈 값을 입력했습니다.");
+        }
     }
 }
