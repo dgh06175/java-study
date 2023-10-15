81742f4fb0f75c7668d5e7595520cf9e676c74a0
dgh06175 <dgh06175@gmail.com>
2023-10-16 00:49:04 +0900

    feat: 금액 입력 받는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -4,36 +4,42 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.*;
 
 public class LottoGame {
+    private static final int INVALID_MONEY = -1;
+    private static final int LOTTO_PRICE = 1000;
+
     private Lotto winningLotto;
     private int money;
     private List<Lotto> lottoArray;
-    private LottoMachine lottoMachine;
-    private LottoAnalyzer lottoAnalyzer;
 
     public void play() {
-        inputMoney();
-        //lottoMachine = new LottoMachine(money);
-        //lottoArray = lottoMachine.getLottoArray();
-        //inputWinningLotto();
-        //lottoAnalyzer = new LottoAnalyzer(lottoArray, winningLotto);
-        //printResult(lottoAnalyzer.getResult());
+        money = inputMoney();
+        if (money == INVALID_MONEY) return;
+        System.out.println("입력한 금액은 "+money);
+        //lottoArray = new Lotto(money);
     }
 
-    private void inputMoney() {
+    private int inputMoney() {
         String inputMoney = Console.readLine();
         try {
-            validateInputMoney(inputMoney);
+            return validateInputMoney(inputMoney);
         } catch (IllegalArgumentException e) {
-            // e.printStackTrace();
             System.out.println(e.getMessage());
+            return INVALID_MONEY;
         }
     }
 
-    private void validateInputMoney(String inputMoney) throws IllegalArgumentException {
+    private int validateInputMoney(String inputMoney) throws IllegalArgumentException {
+        int parsedMoney;
         try {
-            Integer.parseInt(inputMoney);
+            parsedMoney = Integer.parseInt(inputMoney);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 돈 값을 입력했습니다.");
         }
+
+        if (parsedMoney < 1000) {
+            throw new IllegalArgumentException("[ERROR] 1000 미만의 값을 입력했습니다.");
+        }
+
+        return parsedMoney;
     }
 }
