a40ee071fee31c587342a009069b22a3f76dbf22
dgh06175 <dgh06175@gmail.com>
2023-10-16 19:53:30 +0900

    feat: Lotto 테스트 통과하도록 검증 코드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,21 +1,25 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validate2(numbers);
         Collections.sort(numbers);
         this.numbers = new ArrayList<>(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validate2(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
             throw new IllegalArgumentException();
         }
     }
