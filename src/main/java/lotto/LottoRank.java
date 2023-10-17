7dd65cbe98040ddbe5fe91c3b9a344ddc454712d
dgh06175 <dgh06175@gmail.com>
2023-10-17 20:35:00 +0900

    feat: enum 선언 추가

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,4 +1,38 @@
 package lotto;
 
-public enum LottoRank {
+import java.text.DecimalFormat;
+
+enum LottoRank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THRID(5, false, 1500000),
+    FORTH(4, false, 50000),
+    FIFTH(3, false, 5000);
+
+    private final int match;
+    private final boolean isBonusMatch;
+    private final int price;
+    DecimalFormat formatter = new DecimalFormat("###, ###");
+
+    LottoRank (int match, boolean isBonusMatch, int price) {
+        this.match = match;
+        this.isBonusMatch = isBonusMatch;
+        this.price = price;
+    }
+
+    int getMatch() {
+        return match;
+    }
+
+    boolean getIsBonusMatch() {
+        return isBonusMatch;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    void printRankInfo() {
+        System.out.println(match + "개 일치 (" + formatter.format(price) + "원)");
+    }
 }
